cdc6426165cffb11046ce62b0d46acb760ded5f0
JB_Face <33738162+JB-Face@users.noreply.github.com>
2018-03-19 17:02:14 +0800

    Update 星云create.py

--- 星云create.py
+++ 星云create.py
@@ -1,56 +1,69 @@
 import maya.cmds as cmds
+import os
 import random
-from pymel.core import *
 import math
 
-pnumber = 1000#粒子数目
-long = 50#粒子长宽
-width = 1#粒子高
-eel = []
-name = 'ship'
-vortname = 'vortexField1'
 
-agle = 22.5#角度
-fortime = 180//agle#数量-1
-abgle = (math.pi/180)* agle
-kkk = int(fortime)-1
+class AR_poly(object):
+    use = None
 
+    def showUI(cls, uifile):
+        win = cls(uifile)
+        win.create()
+        return win
 
+    def __init__(self, filepath):
+        AR_poly.use = self
+        self.window = '0000'
+        self.uifile = filepath
 
+    def create(self):
+        if cmds.window(self.window, exists=True):
+            cmds.deleteUI(self.window)
 
-        
-    
-
-def create(pname):
-
-    for i in range(pnumber):
-        eel.append ((random.uniform(-1,1),random.uniform(-2,2),random.uniform(-1*long,long)))
-    #for i in range(pnumber):
-        #eel.append ((random.uniform(-1*long,long),random.uniform(-2,2),random.uniform(-1,1)))
-    for i in range(kkk):
-        for i in range(pnumber):
-            cos = math.cos(abgle * i)
-            sin = math.sin(abgle * i)
-            eel.append((eel[i][0]*cos+eel[i][2]*sin,eel[i][1],eel[i][2]*cos-eel[i][0]*sin))
-           
-         
-    
-    cmds.particle(jbp = eel,n = name,c = 1)
-
-    cmds.setAttr("%sShape.particleRenderType"%name,4)
-
-    
+        self.window = cmds.loadUI(uiFile=self.uifile, verbose=True)
+        cmds.showWindow(self.window)
 
 
-def link (pname,vname):
-    cmds.vortex(n = vortname,pos = (0,0,0),m = 5,ay = 1,att = 1,mxd = -1,vsw = 360,tsr = 0.5)
-    cmds.connectAttr('%s.outputForce[0]'%vname,'%sShape.inputForce[0]'%pname)
-    cmds.connectAttr('%sShape.fieldData'%pname,'%s.inputData[0]'%vname)
-    cmds.connectAttr('%sShape.ppFieldData[0]'%pname,'%s.inputPPData[0]'%vname)
+class tplink(object):
+    use = None
 
-    
-create(name)
+    def __init__(self, time, pname, long, kkk, pnumber,  shipname='ship'):
+        self.time = time
+        self.pname = pname
+        self.long = long
+        self.kkk = kkk
+        self.pnumber = pnumber
+        #self.abgle = abgle
+        self.name = shipname
+        self.eel = []
+        tplink.use = self
 
-       
-link(name,vortname)
+    def run(self):
+        cmds.polyCube(h=self.time)
 
+    def create(self, *args):
+        abgle = None
+        try:
+            path = '|'.join(['box','getagle'])
+            abgle = float(cmds.textField(path,q=True,text=True))
+        except:raise 
+
+
+        for i in range(self.pnumber):
+            self.eel.append((random.uniform(-1, 1), random.uniform(-2, 2), random.uniform(-1 * self.long, self.long)))
+        for i in range(self.kkk):
+            for i in range(self.pnumber):
+                cos = math.cos(abgle * i)
+                sin = math.sin(abgle * i)
+                self.eel.append((self.eel[i][0] * cos + self.eel[i][2] * sin, self.eel[i][1],
+                                 self.eel[i][2] * cos - self.eel[i][0] * sin))
+
+        cmds.particle(jbp=self.eel, n=self.name, c=1)
+
+        cmds.setAttr("%sShape.particleRenderType" % self.name, 4)
+
+
+opcc = tplink(time=3, pname='fut', long=20, kkk=4, pnumber=500)
+win = AR_poly(os.path.join(os.getenv('HOME'), 'poly.ui'))
+win.create()
